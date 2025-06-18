8212178b18af11cb06c29c33d99b5513c0fcc619
RichterChr <richterchr@gmx.net>
2025-06-18 18:40:17 +0200

    Draw the player Spaceship

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
+    pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    FPS = pygame.time.Clock()
+    Spaceship = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     begin_loop = 1
     while True:
          if begin_loop == 1:
@@ -14,7 +19,11 @@ def main():
                 if event.type == pygame.QUIT:
                     return
             screen.fill((0,0,0))
+            Spaceship.draw(screen)
+            dt = FPS.tick(60) / 1000
             pygame.display.flip()
+            
+            
 
 
 

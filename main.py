7dec66f36ce002212593e808b2be9c23b3213b62
RichterChr <richterchr@gmx.net>
2025-06-18 17:16:12 +0200

    fixed spelling error

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from constants import *
 
 def main():
     print("Starting Asteroids!")
-    print(f"Screen width = {SCREEN_WIDTH}")
-    print(f"Screen height = {SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 
